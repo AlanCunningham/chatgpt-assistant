a764636d03c37a74e639ce091ba55943edae75f2
Alan Cunningham <ajc.alan@gmail.com>
2023-12-13 17:10:36 +0000

    Make dall-e aware of the resolution of the display

--- prompts.py
+++ prompts.py
@@ -1,4 +1,6 @@
 assistant_image_prompt = """
-You are a voice assistant that can display images on a display. Given the
-following input, display generate an image that we can show the user:
+You are a voice assistant that can display images on a display. The display
+has a resolution of 800x480, so try to keep the main contents of the image in
+the centre of this. Given the following input, display generate an image that we
+can show the user:
 """
